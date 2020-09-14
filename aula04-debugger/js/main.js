597a9df879cee5c906658e2199f9a55d1a5f4406
AnaVatte1 <69902265+AnaVatte1@users.noreply.github.com>
2020-09-13 21:32:12 -0300

    Aula04 Depurando código com Javascript

--- aula04-debugger/js/main.js
+++ aula04-debugger/js/main.js
@@ -4,6 +4,12 @@
 let entradas = document.querySelectorAll('input');
 //Botão somar
 let btnSomar = document.querySelector('#btnSomar');
+//Botão Subtrair
+let btnSubtrair = document.querySelector("#btnSubtrair");
+//Botão Multiplicar
+let btnMultiplicar = document.querySelector("#btnMultiplicar");
+//Botão Dividdir
+let btnDividir = document.querySelector("#btnDividir");
 //Parágrafo de resposta
 let resposta = document.querySelector('.resposta')
  
@@ -38,8 +44,38 @@ function somar(){
    }
 }
 
+//Função Subtrair
+function subrtrair(){
+   if(verificarEntradasVazias()){
+      atualizaResposta2();
+   }else{
+      atualizaResposta2();
+   }
+}
+
+//Função Multiplicar
+function multiplicar(){
+   if(verificarEntradasVazias()){
+      atualizaResposta3();
+   }else{
+      atualizaResposta3();
+   }
+}
+
+//Função Dividir
+function dividir(){
+   if(verificarEntradasVazias()){
+      atualizaResposta4();
+   }else{
+      atualizaResposta4();
+   }
+}
+
 //Escuta de evento click do mouse
 btnSomar.addEventListener('click',somar);
+btnSubtrair.addEventListener('click',subrtrair);
+btnMultiplicar.addEventListener('click',multiplicar);
+btnDividir.addEventListener('click',dividir);
  
 //Atualizar o campo de resposta de acordo com a função
 function atualizaResposta() {
@@ -57,6 +93,60 @@ function atualizaResposta() {
        numero2 = parseFloat(numero2); //Convertendo para número Real
        let soma = numero1 + numero2;
        resposta.textContent = `${numero1} + ${numero2} = ${soma}`;
+   }
+}
+
+function atualizaResposta2() {
+   if(verificarEntradasVazias()){
+        resposta.classList.remove('certa');
+        resposta.classList.add('errada');
+        //debugger;
+        resposta.textContent = "Erro: Preencha os dois números!"
+   }else{
+        resposta.classList.remove('errada');
+        resposta.classList.add('certa');
+       let numero1 = pegarNum1();
+       numero1 = parseFloat(numero1); //Convertendo para número Real
+       let numero2 = pegarNum2();
+       numero2 = parseFloat(numero2); //Convertendo para número Real
+       let subrtrair = numero1 - numero2;
+       resposta.textContent = `${numero1} - ${numero2} = ${subrtrair}`;
+   }
+}
+
+function atualizaResposta3() {
+   if(verificarEntradasVazias()){
+        resposta.classList.remove('certa');
+        resposta.classList.add('errada');
+        //debugger;
+        resposta.textContent = "Erro: Preencha os dois números!"
+   }else{
+        resposta.classList.remove('errada');
+        resposta.classList.add('certa');
+       let numero1 = pegarNum1();
+       numero1 = parseFloat(numero1); //Convertendo para número Real
+       let numero2 = pegarNum2();
+       numero2 = parseFloat(numero2); //Convertendo para número Real
+       let multiplicar = numero1 * numero2;
+       resposta.textContent = `${numero1} * ${numero2} = ${multiplicar}`;
+   }
+}
+
+function atualizaResposta4() {
+   if(verificarEntradasVazias()){
+        resposta.classList.remove('certa');
+        resposta.classList.add('errada');
+        //debugger;
+        resposta.textContent = "Erro: Preencha os dois números!"
+   }else{
+        resposta.classList.remove('errada');
+        resposta.classList.add('certa');
+       let numero1 = pegarNum1();
+       numero1 = parseFloat(numero1); //Convertendo para número Real
+       let numero2 = pegarNum2();
+       numero2 = parseFloat(numero2); //Convertendo para número Real
+       let dividir = numero1 / numero2;
+       resposta.textContent = `${numero1} / ${numero2} = ${dividir}`;
    }
 }
 
